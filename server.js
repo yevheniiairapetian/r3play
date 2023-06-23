2b25d3569f11fbbbb64b73d62918039ea62a5944
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-06-23 16:59:02 +0200

    fix: fix file placement, fix server function

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ http.createServer((request, response) => {
         filePath = 'index.html';
 
       }
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Node!\n');
-  }).listen(8080);
+
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          throw err;
+        }
+      
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
+    response.end();
+  })
+}).listen(8090)
