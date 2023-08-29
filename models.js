1fb1b821984697f6591fc5cd78404d82cd2eca21
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-08-29 23:24:12 +0200

    feat: update models

--- models.js
+++ models.js
@@ -19,12 +19,32 @@ let movieSchema = mongoose.Schema({
 
 });
 
+let TVseriesSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Season: {},
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Bio: String
+  },
+  ImagePath: String,
+  Featured: Boolean,
+  Actors: [String],
+  Rating: String,
+  ReleaseDate: Date
+
+});
+
 let userSchema = mongoose.Schema({
   Username: { type: String, required: true, unique: true },
   Password: { type: String, required: true },
   Email: { type: String, required: true, unique: true },
   Birthday: Date,
-  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie', ref: 'TVseries' }]
 });
 
 userSchema.statics.hashPassword = (password) => {
@@ -36,7 +56,9 @@ userSchema.methods.validatePassword = function (password) {
 };
 
 let Movie = mongoose.model('Movie', movieSchema);
+let TVseries = mongoose.model('TVseries', TVseriesSchema);
 let User = mongoose.model('User', userSchema);
 
 module.exports.Movie = Movie;
+module.exports.TVseries = TVseries;
 module.exports.User = User;
