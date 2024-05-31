0e0714b1884f8d61d2c691b9cf2ab08b11158e0d
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-05-31 20:46:50 +0200

    test: comment in test code

--- index.js
+++ index.js
@@ -540,17 +540,17 @@ app.put("/users/:id", passport.authenticate('jwt', { session: false }), [
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
   check('Password', 'Password is required').not().isEmpty(),
   check('Email', 'Email does not appear to be valid').isEmail(),
-  upload(req, res, (err) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: err });
-     }
-    if (!req.file) {
-       return res.status(400).json({ error: 'Please send file' });
-     }
-     console.log(req.file);
-     res.send('File uploaded!');
- }),
+//   upload(req, res, (err) => {
+//     if (err) {
+//       console.error(err);
+//       return res.status(500).json({ error: err });
+//      }
+//     if (!req.file) {
+//        return res.status(400).json({ error: 'Please send file' });
+//      }
+//      console.log(req.file);
+//      res.send('File uploaded!');
+//  }),
   ], async (req, res) => {
   let errors = validationResult(req);
   
