39ae57b5f19aa0ff45fcc817816028788e150561
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-06-01 14:56:42 +0200

    feat: add watched movie feature. test: test watched tv series feature

--- models.js
+++ models.js
@@ -92,7 +92,8 @@ let userSchema = mongoose.Schema({
   Email: { type: String, required: true, unique: true },
   Birthday: Date,
   WatchedMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie', 
-  // ref: 'TVseries', ref: 'Anime'
+  ref: 'TVseries', 
+  // ref: 'Anime'
  }],
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie',
    ref: 'TVseries', ref: 'Anime' 
